b36860cfb902e43b96660d3a3b50a878190f3954
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-05-02 16:51:12 +0200

    Make frozen players unable to manage inventories

--- src/main/java/pl/olafcio/expandedbans/main/listeners/FreezeListener.java
+++ src/main/java/pl/olafcio/expandedbans/main/listeners/FreezeListener.java
@@ -2,7 +2,10 @@ package pl.olafcio.expandedbans.main.listeners;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.PlayerInventory;
 import org.jetbrains.annotations.ApiStatus;
 import pl.olafcio.expandedbans.ExpandedBans;
 
@@ -10,6 +13,24 @@ import pl.olafcio.expandedbans.ExpandedBans;
 public class FreezeListener implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onMove(PlayerMoveEvent event) {
+        var player = event.getPlayer();
+        var uuid = player.getUniqueId();
+
+        if (ExpandedBans.Players.get(uuid).isFrozen())
+            event.setCancelled(true);
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onInventoryClick(InventoryClickEvent event) {
+        var player = ((PlayerInventory) event.getInventory()).getHolder();
+        var uuid = player.getUniqueId();
+
+        if (ExpandedBans.Players.get(uuid).isFrozen())
+            event.setCancelled(true);
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onPlayerDropItem(PlayerDropItemEvent event) {
         var player = event.getPlayer();
         var uuid = player.getUniqueId();
 
