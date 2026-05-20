997cc54cf727bd7f27b3ae8389cb04f0559201de
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-05-20 22:44:07 +0200

    Fix FreezeListener#onInventoryClick

--- src/main/java/pl/olafcio/expandedbans/main/listeners/FreezeListener.java
+++ src/main/java/pl/olafcio/expandedbans/main/listeners/FreezeListener.java
@@ -1,11 +1,11 @@
 package pl.olafcio.expandedbans.main.listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.inventory.PlayerInventory;
 import org.jetbrains.annotations.ApiStatus;
 import pl.olafcio.expandedbans.ExpandedBans;
 
@@ -22,11 +22,13 @@ public class FreezeListener implements Listener {
 
     @EventHandler(ignoreCancelled = true)
     public void onInventoryClick(InventoryClickEvent event) {
-        var player = ((PlayerInventory) event.getInventory()).getHolder();
-        var uuid = player.getUniqueId();
+        var holder = event.getInventory().getHolder();
+        if (holder instanceof Player player) {
+            var uuid = player.getUniqueId();
 
-        if (ExpandedBans.Players.get(uuid).isFrozen())
-            event.setCancelled(true);
+            if (ExpandedBans.Players.get(uuid).isFrozen())
+                event.setCancelled(true);
+        }
     }
 
     @EventHandler(ignoreCancelled = true)
